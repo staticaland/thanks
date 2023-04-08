0b064d3c24466299186da9023f833b210223d725
Anders K. Pettersen <anders@const.dev>
2023-04-08 01:09:58 +0000

    Add OpenAI LLM and travel prompt function to dog.py

--- dog.py
+++ dog.py
@@ -4,17 +4,42 @@ from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage, SystemMessage, AIMessage
 from langchain.schema import Document
 from langchain.document_loaders import HNLoader
+from langchain.llms import OpenAI
+from langchain import PromptTemplate
 
 chat = ChatOpenAI(temperature=0.7, openai_api_key=os.getenv("OPENAI_API_KEY"))
 loader = HNLoader("https://news.ycombinator.com/item?id=35484594")
 data = loader.load()
 # TODO: Find out how to use this
 
+# Seems that selectors are like table driven tests
+
+def travel_prompt(location):
+    prompt = PromptTemplate(
+        input_variables=["location"],
+        template="""
+        I really want to travel to {location}. What should I do there?
+
+        Respond in one short sentence
+        """,
+    )
+    return prompt.format(location=location)
+
 
 def main():
+    llm = OpenAI(
+        model_name="text-davinci-003", openai_api_key=os.getenv("OPENAI_API_KEY")
+    )
+    final_prompt = travel_prompt("New York")
+    print(f"Final Prompt: {final_prompt}")
+    print("-----------")
+    print(f"LLM Output: {llm(final_prompt)}")
+    exit()
+
     print(chat([HumanMessage(content="Hello")]))
     print(chat([HumanMessage(content="How are you?")]))
     print(chat([HumanMessage(content="What's your name?")]))
+    print(chat([HumanMessage(content="Heads or tails?")]))
     print(
         chat(
             [
